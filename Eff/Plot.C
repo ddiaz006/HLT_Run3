edc73359ab233cb74609bc3310558030fe162fb1
Daniel Diaz <ddiaz006@ucr.edu>
2022-04-06 10:42:34 +0200

    Auto Plots

--- Eff/Plot.C
+++ Eff/Plot.C
@@ -17,6 +17,13 @@
 
 void Plot(){
 
+TString SampleName = "_GluGluToHHTo2B2Tau_node_cHHH1";
+TFile *_file0 = TFile::Open("histograms"+SampleName+".root");
+std::cout<<"open root file"<<std::endl;
+TString outpath = "plots"+SampleName;
+TString mdcommand = (TString)"mkdir -p "+outpath.Data();
+const int dir_err = system(mdcommand);
+
 TString name[817];
 std::fstream names;
 names.open("trigger_names_llp_ForRun3HLTStudy.txt", std::fstream::in);
@@ -30,30 +37,7 @@ if (names.is_open()) {
             }
 }
 names.close();
-// TText* title = new TText(1,1,"") ;
-// title->SetTextSize(0.04);
-// title->SetTextColor(kBlack);
-// title->SetTextAlign(11);
-// title->SetTextFont(62);
-//
-// TText* extra = new TText(1,1,"") ;
-// extra->SetTextSize(0.04);
-// extra->SetTextColor(kBlack);
-// extra->SetTextAlign(11);
-// extra->SetTextFont(52);
-//
-// TText* extra2 = new TText(1,1,"") ;
-// extra2->SetTextSize(0.04);
-// extra2->SetTextColor(kBlack);
-// extra2->SetTextAlign(11);
-// extra2->SetTextFont(62);
-//
-// TText* lumi = new TText(1,1,"") ;
-// lumi->SetTextSize(0.03);
-// lumi->SetTextColor(kBlack);
-// lumi->SetTextAlign(31);
-// lumi->SetTextFont(42);
-//
+
  TCanvas* canvas = new TCanvas("canvas","canvas",900,100,500,500); 
  canvas->Clear();
  canvas->cd();
@@ -66,10 +50,10 @@ names.close();
  gStyle->SetPalette(kBird);
  gStyle->SetLineWidth(2);
 
- TFile *_file0 = TFile::Open("histograms.root");
-std::cout<<"open root file"<<std::endl;
  TH1F* TTOCNum_PT[817];  
  TH1F* TTOCDen_PT[817];  
+ TH1F* TTOCNum_Mass[817];  
+ TH1F* TTOCDen_Mass[817];  
  TGraphAsymmErrors* gr_TTOC_PT[817]; //= new TGraphAsymmErrors();
  TGraphAsymmErrors* gr_TTOC_Mass[817]; //= new TGraphAsymmErrors();
 
@@ -77,8 +61,11 @@ std::cout<<"open root file"<<std::endl;
  
  char titleNum[100];
  char titleDen[100];
- for (int k = 0; k<817; k++){
-   
+ char histName[100];
+ //Plot all PT Turn ons
+ for (int k = 0; k<143; k++){
+   //if(k >143) continue; //only for Triggers we care about
+   // Pt Histos
    sprintf(titleNum,"h_PT[%d]_Num",k);
    sprintf(titleDen,"h_PT[%d]_Den",k);
    TTOCNum_PT[k] = (TH1F*)_file0->Get(titleNum)->Clone(titleNum);
@@ -87,10 +74,37 @@ std::cout<<"open root file"<<std::endl;
    gr_TTOC_PT_->Divide(TTOCNum_PT[k], TTOCDen_PT[k], "cl=0.683 b(1,1) mode");
    gr_TTOC_PT[k] = gr_TTOC_PT_;
    
+   // Mass Histos
+   sprintf(titleNum,"h_Mass[%d]_Num",k);
+   sprintf(titleDen,"h_Mass[%d]_Den",k);
+   TTOCNum_Mass[k] = (TH1F*)_file0->Get(titleNum)->Clone(titleNum);
+   TTOCDen_Mass[k] = (TH1F*)_file0->Get(titleDen)->Clone(titleDen);
+   TGraphAsymmErrors* gr_TTOC_Mass_ = new TGraphAsymmErrors(); 
+   gr_TTOC_Mass_->Divide(TTOCNum_Mass[k], TTOCDen_Mass[k], "cl=0.683 b(1,1) mode");
+   gr_TTOC_Mass[k] = gr_TTOC_Mass_;
+   
+
+   //Plotting
    gr_TTOC_PT[k]->GetXaxis()->SetTitle("AK8JetPT GeV");
    gr_TTOC_PT[k]->SetTitle(name[k]);
    gr_TTOC_PT[k]->SetMaximum(1.0);
+   gr_TTOC_Mass[k]->GetXaxis()->SetTitle("AK8JetSoftDropMass GeV");
+   gr_TTOC_Mass[k]->SetTitle(name[k]);
+   gr_TTOC_Mass[k]->SetMaximum(1.0);
+
+   sprintf(histName,"Eff_PT[%d]",k);
+   gr_TTOC_PT[k]->Draw();   
+   canvas->SaveAs(outpath+"/"+histName+".pdf");
+   canvas->Clear(); 
+  
+   sprintf(histName,"Eff_Mass[%d]",k);
+   gr_TTOC_Mass[k]->Draw();   
+   canvas->SaveAs(outpath+"/"+histName+".pdf");
+   canvas->Clear(); 
  }
+
+
+ //Special Plots
  TLegend *leg;
  leg = new TLegend(0.15,0.6,0.5,0.8);
  leg->SetBorderSize(0);
@@ -112,7 +126,7 @@ std::cout<<"open root file"<<std::endl;
  gr_TTOC_PT[22]->Draw("sames"); //SD 50
  gr_TTOC_PT[0]->Draw("sames"); //Trim
  leg->Draw("sames");
- canvas->SaveAs("plots/1JPT360AllMass_compare.pdf");
+ canvas->SaveAs("plots"+SampleName+"/1JPT360AllMass_compare.pdf");
  //canvas->Clear();
  leg->Clear();
 
@@ -125,7 +139,7 @@ std::cout<<"open root file"<<std::endl;
  gr_TTOC_PT[4]->Draw(); //SD 30
  gr_TTOC_PT[0]->Draw("sames"); //Trim
  leg->Draw("sames");
- canvas->SaveAs("plots/1JPT360Mass30-30_compare.pdf");
+ canvas->SaveAs("plots"+SampleName+"/1JPT360Mass30-30_compare.pdf");
  //canvas->Clear();
  leg->Clear();
 
@@ -137,7 +151,7 @@ std::cout<<"open root file"<<std::endl;
  gr_TTOC_PT[13]->Draw(); //SD 40
  gr_TTOC_PT[0]->Draw("sames"); //Trim
  leg->Draw("sames");
- canvas->SaveAs("plots/1JPT360Mass30-40_compare.pdf");
+ canvas->SaveAs("plots"+SampleName+"/1JPT360Mass30-40_compare.pdf");
  //canvas->Clear();
  leg->Clear();
 
@@ -149,7 +163,7 @@ std::cout<<"open root file"<<std::endl;
  gr_TTOC_PT[22]->Draw(); //SD 50
  gr_TTOC_PT[0]->Draw("sames"); //Trim
  leg->Draw("sames");
- canvas->SaveAs("plots/1JPT360Mass30-50_compare.pdf");
+ canvas->SaveAs("plots"+SampleName+"/1JPT360Mass30-50_compare.pdf");
  //canvas->Clear();
  leg->Clear();
 
